95f109fb8e52d1910be367024d7a7f9527c1c747
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-08 23:11:10 +0300

    add more style

--- src/components/FolderList.jsx
+++ src/components/FolderList.jsx
@@ -1,21 +1,29 @@
 import TreeData from './TreeData.js'
 import { FaFolderOpen } from "react-icons/fa";
+import './FolderList.css'
 
 function FolderList() {
-  return (
-    <div>
-      {TreeData && TreeData.length > 0 ? (
-        TreeData.map((folders, index) => (
-          <p key={index}>
-            <FaFolderOpen />
-            {folders.label}
-          </p>
-        ))
-      ) : (
-        <p>No Folders to display</p>
-      )}
-    </div>
-  );
+    return (
+        <div className="side vh-100 p-3" style={{ width: '250px' }}>
+            <h2>Computer</h2>
+            {TreeData && TreeData.length > 0 ? (
+                TreeData.map((folders, index) => (
+                    <ul key={index} className="nav flex-column">
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#">
+                                <span>
+                                    <FaFolderOpen />
+                                </span>
+                                <span className=''>{folders.label}</span>
+                            </a>
+                        </li>
+                    </ul>
+                ))
+            ) : (
+                <p>No Folders to display</p>
+            )}
+        </div>
+    );
 }
 
 export default FolderList
