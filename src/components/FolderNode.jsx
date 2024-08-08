2b3591c90d191571a7ef62712f2e161227671dd1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-09 02:06:40 +0300

    add padding to levels

--- src/components/FolderNode.jsx
+++ src/components/FolderNode.jsx
@@ -7,28 +7,20 @@ function FolderNode({ item, level = 0 }) {
 
   return (
     <ul className="nav flex-column">
-      <li className="nav-item">
+      <li className={`nav-item`} style={{paddingLeft: `${level * 20}px`}}>
         <a className="" href="#">
-          <span>
-            {
-              childrenPresent && <span>{open ? "+" : "-"}</span>
-            }
-          </span>
+          <span>{childrenPresent && <span>{open ? "+" : "-"}</span>}</span>
           <span>
             <FaFolderOpen />
           </span>{" "}
           {item.label}
-          {
-            childrenPresent && (
-              <div className={`${open ? 'show' : 'hide'}`}>
-                {
-                  item.children.map((child, index) => (
-                    <FolderNode key={index} item={child} level={level + 1} />
-                  ))
-                }
-              </div>
-            )
-          }
+          {childrenPresent && (
+            <div className={`${open ? "show" : "hide"}`}>
+              {item.children.map((child, index) => (
+                <FolderNode key={index} item={child} level={level + 1} />
+              ))}
+            </div>
+          )}
         </a>
       </li>
     </ul>
