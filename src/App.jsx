3126efb7e19ec1e7d45e13aabef500dc40970bb2
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-09 01:11:50 +0300

    add folder to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import FolderList from './components/FolderList'
+import treeData from './components/treeData';
+
 
 function App() {
 
   return (
-    <>
-      <FolderList />
-    </>
-  )
+    <div className="side vh-100 p-3" style={{ width: "250px" }}>
+      <h2>Computer</h2>
+      <FolderList data={treeData} />
+    </div>
+  );
 }
 
 export default App
