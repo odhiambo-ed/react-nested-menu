e8ee5dda7f66a980d0672103e03fa13ad7d8af8e
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-08 22:07:09 +0300

    add folder icon

--- src/components/FolderList.jsx
+++ src/components/FolderList.jsx
@@ -1,19 +1,21 @@
 import TreeData from './TreeData.js'
+import { FaFolderOpen } from "react-icons/fa";
 
 function FolderList() {
   return (
-      <div>
-          {
-              TreeData && TreeData.length > 0 ? (
-                  TreeData.map((folders, index) => (
-                      <p key={index}>{ folders.label }</p>
-                  ))
-              ) : (
-                      <p>No Folders to display</p>
-              )
-          }
+    <div>
+      {TreeData && TreeData.length > 0 ? (
+        TreeData.map((folders, index) => (
+          <p key={index}>
+            <FaFolderOpen />
+            {folders.label}
+          </p>
+        ))
+      ) : (
+        <p>No Folders to display</p>
+      )}
     </div>
-  )
+  );
 }
 
 export default FolderList
