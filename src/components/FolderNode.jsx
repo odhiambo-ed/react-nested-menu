ca61aca64e3dc9dd1455909fb4853c6710c00148
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-09 01:52:39 +0300

    add icon to folder

--- src/components/FolderNode.jsx
+++ src/components/FolderNode.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import { FaFolderOpen } from "react-icons/fa";
 
+function FolderNode({ item, level = 0 }) {
+  const [open, setOpen] = useState(false)
+  const childrenPresent = item.children && item.children.length > 0
 
-function FolderNode() {
   return (
-    <div>Folder</div>
-  )
+    <ul className="nav flex-column">
+      <li className="nav-item">
+        <a className="" href="#">
+          <span>
+            {
+              childrenPresent && <span>{open ? "+" : "-"}</span>
+            }
+          </span>
+          <span>
+            <FaFolderOpen />
+          </span>{" "}
+          {item.label}
+          {
+            
+          }
+        </a>
+      </li>
+    </ul>
+  );
 }
 
 export default FolderNode
