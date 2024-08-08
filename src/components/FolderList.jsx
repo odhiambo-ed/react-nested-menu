a319b17f46f4d0df1af2ad5e5c7036e615ff4a37
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-09 01:52:11 +0300

    edit folder list

--- src/components/FolderList.jsx
+++ src/components/FolderList.jsx
@@ -1,29 +1,13 @@
-import TreeData from './TreeData.js'
-import { FaFolderOpen } from "react-icons/fa";
 import './FolderList.css'
-import { useState } from 'react';
+import FolderNode from "./FolderNode";
 
-function FolderList() {
-    const [open, setOpen] = useState(false)
+function FolderList({ data }) {
     return (
-        <div className="side vh-100 p-3" style={{ width: '250px' }}>
-            <h2>Computer</h2>
-            {TreeData && TreeData.length > 0 ? (
-                TreeData.map((folders, index) => (
-                    <ul key={index} className="nav flex-column">
-                        <li className="nav-item">
-                            <a className="" href="#">
-                                <span>
-                                    <FaFolderOpen />
-                                </span> {folders.label}
-                            </a>
-                        </li>
-                    </ul>
-                ))
-            ) : (
-                <p>No Folders to display</p>
-            )}
-        </div>
+        <>
+            {data.map((item, index) => (
+                <FolderNode key={index} item={item} />
+            ))}
+        </>
     );
 }
 
